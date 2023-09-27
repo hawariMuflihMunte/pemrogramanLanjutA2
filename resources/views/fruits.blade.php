14a97a9bcef1bd72ee789da53797097ee01c9f85
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-09-27 17:25:45 +0700

    refactor(views): fruits

--- resources/views/fruits.blade.php
+++ resources/views/fruits.blade.php
@@ -5,14 +5,23 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Fruits</title>
+
+  <link rel="stylesheet" href="https://cdn.korzh.com/metroui/v4.5.1/css/metro-all.min.css">
+
+  <link rel="stylesheet" href="{{ asset('assets/css/normalize.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/skeleton.css') }}">
+
 </head>
 <body>
-  <h1>Fruits List</h1>
-  <h3>Total Fruits: {{ $totalCount }}</h3>
-  <ul>
-    @foreach ($fruits as $fruit)
-      <li>{{ $fruit }}</li>
-    @endforeach
-  </ul>
+  <main class="p-5 border-solid border-size-1">
+    <h1>Fruits List</h1>
+    <h3>Total Fruits: {{ $totalCount }}</h3>
+    <a class="button" href="{{ route('add') }}">Add Fruits +</a>
+    <ul>
+      @foreach ($fruits as $fruit)
+        <li>{{ $fruit['name'] }} - ({{ date('m/d/Y',  strtotime($fruit['updated_at'])) }})</li>
+      @endforeach
+    </ul>
+  </main>
 </body>
 </html>
