31107d38d91e948239dfee91609a37385fdec1a2
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-09-27 17:23:41 +0700

    refactor(controller): data privacy

--- app/Http/Controllers/StudentDataController.php
+++ app/Http/Controllers/StudentDataController.php
@@ -6,5 +6,37 @@ use Illuminate\Http\Request;
 
 class StudentDataController extends Controller
 {
-    //
+    private $data;
+
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    public function setData($data)
+    {
+        $this->data = $data;
+    }
+
+    public function __invoke()
+    {
+        $this->data = [
+            "nim" => "200170120",
+            "nama" => "Hawari Muflih Munte",
+            "jurusan" => "Teknik Informatika",
+            "programStudi" => "Teknik Informatika",
+            "mataKuliah" => "Pemrograman Lanjut A2",
+            "mataKuliahSemesterIni" => [
+                "Pemrograman Lanjut",
+                "Database Terdistribusi",
+                "Internet of Things",
+                "Riset Teknologi Informasi",
+                "Mobile Programming"
+            ]
+        ];
+
+        return view('studentdata', [
+            'studentData' => $this->getData()
+        ]);
+    }
 }
