f18c327396e090b6de292e016d5b8034239aa4d3
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-09-27 10:57:22 +0700

    refactor(ui): change title and h1 in `student_data.blade.php`

--- resources/views/student_data.blade.php
+++ resources/views/student_data.blade.php
@@ -4,11 +4,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Student Data</title>
+  <title>Datadiri</title>
 </head>
 <body>
   <fieldset style="max-width: 80%; margin: 0 auto;">
-    <h1>Student Data</h1>
+    <h1>Datadiri</h1>
     <hr>
     <section>
       <p>
