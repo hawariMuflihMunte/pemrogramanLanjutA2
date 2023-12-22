480173758dc98fe2b1b45b10a3451d9b4b1c76c2
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-12-22 15:25:32 +0700

    Check Auth in `login` method

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -15,6 +14,10 @@ class LoginController extends Controller
 
     public function login(Request $request)
     {
+        if (Auth::check()) {
+            return redirect()->route('data-mahasiswa.index');
+        }
+
         $credentials = $request->validate([
             'username' => ['required'],
             'password' => ['required'],
